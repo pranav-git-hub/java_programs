7737b9282fabd481052833bc175a544ce1bb9ea3
Harsh Ponia <harshponia9@gmail.com>
2021-08-20 20:55:41 +0530

    first commit

---
+++ Basics/hello.java
@@ -0,0 +1,25 @@
+package Basics;
+
+// Ex:- 1
+
+public class hello {
+
+    static void func() {
+            System.out.println("hello, World");
+    }
+    // public static void main(String[] args) {
+
+    //     hello.func();
+        
+    // }
+    
+
+
+public static void main(String[] args) {
+    
+    System.out.println("hello , World");
+}
+
+
+
+}
